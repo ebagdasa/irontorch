da9bdc4dd131359c9098302f9001a3aa0bc4678d
Eugene <ebagdasaryan@me.com>
2022-02-06 17:54:08 -0500

    cifar2 | batch_rand100

--- tasks/samplers/batch_sampler.py
+++ tasks/samplers/batch_sampler.py
@@ -47,18 +47,18 @@ class CosineBatchSampler(torch_data.Sampler[List[int]]):
         self.offset = offset
         self.weights_count = self.weights.shape[0]
         self.previous_vector = None
+        current_id = np.random.choice(self.weights_count)
+        self.previous_vector = self.weights[current_id:current_id+1]
 
     def __iter__(self) -> Iterator[List[int]]:
         batch_ids = []
-        if self.previous_vector is None:
-            current_id = np.random.choice(self.weights_count)
-            self.previous_vector = self.weights[current_id:current_id+1]
-        for i in range(self.batch_size):
-            cos_sims = sim_matrix(self.previous_vector, self.weights).squeeze().sort()
-            candidate = np.random.choice(cos_sims.indices[:100])
-            batch_ids.append(candidate + self.offset)
-            self.previous_vector += self.weights[candidate:candidate+1]
-        yield batch_ids
+        for j in self.weights_count // self.batch_size:
+            for i in range(self.batch_size):
+                cos_sims = sim_matrix(self.previous_vector, self.weights).squeeze().sort()
+                candidate = np.random.choice(cos_sims.indices[:100])
+                batch_ids.append(candidate + self.offset)
+                self.previous_vector += self.weights[candidate:candidate+1]
+            yield batch_ids
         #
         #
         # for idx in self.sampler:
