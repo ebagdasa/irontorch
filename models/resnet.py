c060b65101a18f99210b624d8f33c88132e9898d
Eugene <ebagdasaryan@me.com>
2022-05-17 12:01:30 -0400

    cifar_grads_full_0005 | added bn

--- models/resnet.py
+++ models/resnet.py
@@ -126,7 +126,7 @@ class ResNet(Model):
                  zero_init_residual=False,
                  groups=1, width_per_group=64,
                  replace_stride_with_dilation=None,
-                 norm_layer=None):
+                 norm_layer=None, bn_enable=True):
         super(ResNet, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
--- models/resnet_cifar.py
+++ models/resnet_cifar.py
@@ -12,8 +12,9 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, bn_enable=True):
         super(BasicBlock, self).__init__()
+        self.bn_enable = bn_enable
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -30,8 +31,12 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        if self.bn_enable:
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.bn2(self.conv2(out))
+        else:
+            out = F.relu(self.conv1(x))
+            out = self.conv2(out)
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -40,8 +45,9 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, bn_enable=True):
         super(Bottleneck, self).__init__()
+        self.bn_enable = bn_enable
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
@@ -60,38 +66,51 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
+        if self.bn_enable:
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = F.relu(self.bn2(self.conv2(out)))
+            out = self.bn3(self.conv3(out))
+        else:
+            out = F.relu(self.conv1(x))
+            out = F.relu(self.conv2(out))
+            out = self.conv3(out)
         out += self.shortcut(x)
         out = F.relu(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, bn_enable=True):
         super(ResNet, self).__init__()
+        self.bn_enable = bn_enable
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1,
+                                       bn_enable=bn_enable)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2,
+                                       bn_enable=bn_enable)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2,
+                                       bn_enable=bn_enable)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2,
+                                       bn_enable=bn_enable)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride, bn_enable):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, bn_enable))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        if self.bn_enable:
+            out = F.relu(self.bn1(self.conv1(x)))
+        else:
+            out = F.relu(self.conv1(x))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -102,24 +121,29 @@ class ResNet(nn.Module):
         return out
 
 
-def resnet18(pretrained=None, num_classes=None):
-    return ResNet(BasicBlock, [2, 2, 2, 2],  num_classes=num_classes)
+def resnet18(pretrained=None, num_classes=None, bn_enable=True):
+    return ResNet(BasicBlock, [2, 2, 2, 2],  num_classes=num_classes,
+                  bn_enable=bn_enable)
 
 
-def resnet34(pretrained=None, num_classes=None):
-    return ResNet(BasicBlock, [3, 4, 6, 3],  num_classes=num_classes)
+def resnet34(pretrained=None, num_classes=None, bn_enable=True):
+    return ResNet(BasicBlock, [3, 4, 6, 3],  num_classes=num_classes,
+                  bn_enable=bn_enable)
 
 
-def resnet50(pretrained=None, num_classes=None):
-    return ResNet(Bottleneck, [3, 4, 6, 3],  num_classes=num_classes)
+def resnet50(pretrained=None, num_classes=None, bn_enable=True):
+    return ResNet(Bottleneck, [3, 4, 6, 3],  num_classes=num_classes,
+                  bn_enable=bn_enable)
 
 
-def resnet101(pretrained=None, num_classes=None):
-    return ResNet(Bottleneck, [3, 4, 23, 3],  num_classes=num_classes)
+def resnet101(pretrained=None, num_classes=None, bn_enable=True):
+    return ResNet(Bottleneck, [3, 4, 23, 3],  num_classes=num_classes,
+                  bn_enable=bn_enable)
 
 
-def resnet152(pretrained=None, num_classes=None):
-    return ResNet(Bottleneck, [3, 8, 36, 3],  num_classes=num_classes)
+def resnet152(pretrained=None, num_classes=None, bn_enable=True):
+    return ResNet(Bottleneck, [3, 8, 36, 3],  num_classes=num_classes,
+                  bn_enable=bn_enable)
 
 
 def test():
