8fe8462fc65bc02c1034683745fa8e81d9f53029
Eugene <ebagdasaryan@me.com>
2022-06-04 17:21:03 -0400

    ray run

--- ray_training.py
+++ ray_training.py
@@ -1,6 +1,10 @@
 import argparse
 from ray.tune.integration.wandb import WandbLogger
 from ray.runtime_env import RuntimeEnv
+from ray.tune.suggest.hyperopt import HyperOptSearch
+
+import training
+import helper
 
 from helper import Helper
 from training import train, test
@@ -35,19 +39,20 @@ def run(hlpr):
         # hlpr.plot_confusion_matrix(backdoor=False, epoch=epoch)
         backdoor_metrics = test(hlpr, hlpr.task.model, backdoor=True, epoch=epoch)
         logging.disable(logging.NOTSET)
-        tune.report(accuracy=metrics['accuracy'], backdoor_accuracy=backdoor_metrics['accuracy'])
         # hlpr.plot_confusion_matrix(backdoor=True, epoch=epoch)
         # hlpr.save_model(hlpr.task.model, epoch, metrics['accuracy'])
-        # if hlpr.params.multi_objective_metric is not None:
-        #     main_obj = metrics[hlpr.params.multi_objective_metric]
-        #     back_obj = backdoor_metrics[hlpr.params.multi_objective_metric]
-        #     alpha = hlpr.params.multi_objective_alpha
-        #     multi_obj = alpha * main_obj - (1 - alpha) * back_obj
-        #     hlpr.report_dict(dict_report={'multi_objective': multi_obj}, step=epoch)
+        main_obj = metrics[hlpr.params.multi_objective_metric]
+        back_obj = backdoor_metrics[hlpr.params.multi_objective_metric]
+        alpha = hlpr.params.multi_objective_alpha
+        multi_obj = alpha * main_obj - (1 - alpha) * back_obj
+        tune.report(accuracy=metrics['accuracy'], epoch=epoch,
+                    backdoor_accuracy=backdoor_metrics['accuracy'],
+                    multi_objective=multi_obj)
+        # hlpr.report_dict(dict_report={'multi_objective': multi_obj}, step=epoch)
 
 
 def tune_run(config):
-    with open('/home/eugene/backdoors/configs/mnist_params.yaml') as f:
+    with open('/home/eugene/irontorch/configs/mnist_params.yaml') as f:
         params = yaml.load(f, Loader=yaml.FullLoader)
 
     for key, value in config.items():
@@ -62,17 +67,24 @@ if __name__ == '__main__':
         "momentum": tune.uniform(0.1, 0.9),
         "optimizer": tune.choice(['Adam', 'SGD']),
         "lr": tune.loguniform(1e-4, 1e-1),
-        "wandb": {"project": "rayTune", "monitor_gym": True}
+        "label_noise": tune.uniform(0.0, 0.5),
+        "decay": tune.loguniform(5e-7, 5e-3),
+        "epochs": tune.qrandint(5, 15),
+        "batch_size": tune.loguniform(4, 10, 2)
     }
-    runtime_env = RuntimeEnv(
-        conda='pt',
-        working_dir="/home/eugene/irontorch",
-        # py_modules=[Helper, test, train]
+    config={"wandb": {"project": "rayTune", "monitor_gym": True}}
+    # runtime_env = RuntimeEnv(
+    #     conda='pt',
+    #     working_dir="/home/eugene/irontorch",
+    #     # py_modules=[Helper, test, train]
+    #
+    # )
+    hyperopt_search = HyperOptSearch(search_space, metric="mean_accuracy", mode="max")
 
-    )
-    ray.init(address='ray://128.84.84.162:10001', runtime_env=runtime_env)
+    ray.init(address='ray://128.84.84.162:10001', runtime_env={"working_dir": "/home/eugene/irontorch"})
 
-    analysis = tune.run(tune_run, config=search_space, num_samples=10,
+    analysis = tune.run(tune_run, config=config, num_samples=1000,
+                        search_alg=hyperopt_search,
                         # resources_per_trial={'gpu': 1, 'cpu': 2},
                         loggers=[WandbLogger],
                         resources_per_trial=tune.PlacementGroupFactory([{"CPU": 2, "GPU": 1}])
