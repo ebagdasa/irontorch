7e05094b04ee9cfc41b412974451fc0e2451291f
Eugene <ebagdasaryan@me.com>
2022-06-13 08:47:59 -0400

    refactor

--- ray_training.py
+++ ray_training.py
@@ -19,24 +19,34 @@ import functools
 
 def disable_logging(func):
     @functools.wraps(func)
-    def wrapper(*args,**kwargs):
+    def wrapper(*args, **kwargs):
         logging.disable(logging.DEBUG)
-        result = func(*args,**kwargs)
+        result = func(*args, **kwargs)
         logging.disable(logging.NOTSET)
         return result
+
     return wrapper
 
 
-def run(hlpr):
-    # acc = test(hlpr, 0, backdoor=False)
+def run(config):
+    with open(config['file_path']) as f:
+        params = yaml.load(f, Loader=yaml.FullLoader)
+
+    for key, value in config.items():
+        if params.get(key, None) is not None:
+            params[key] = value
+
+    hlpr = Helper(params)
     for epoch in range(hlpr.params.start_epoch,
                        hlpr.params.epochs + 1):
         logging.disable(logging.DEBUG)
         train(hlpr, epoch, hlpr.task.model, hlpr.task.optimizer,
               hlpr.task.train_loader)
         metrics = test(hlpr, hlpr.task.model, backdoor=False, epoch=epoch)
-        drop_class = hlpr.task.metrics['accuracy'].get_value()['_Accuracy_Drop_5']
-        backdoor_metrics = test(hlpr, hlpr.task.model, backdoor=True, epoch=epoch)
+        drop_class = hlpr.task.metrics['accuracy'].get_value()[
+            '_Accuracy_Drop_5']
+        backdoor_metrics = test(hlpr, hlpr.task.model, backdoor=True,
+                                epoch=epoch)
         main_obj = metrics[hlpr.params.multi_objective_metric]
         back_obj = backdoor_metrics[hlpr.params.multi_objective_metric]
         alpha = hlpr.params.multi_objective_alpha
@@ -44,54 +54,81 @@ def run(hlpr):
         tune.report(accuracy=main_obj, drop_class=drop_class,
                     backdoor_accuracy=back_obj,
                     multi_objective=multi_obj, epoch=epoch)
-    # return main_obj, back_obj, multi_obj
-     # main_obj, back_obj
-     #    hlpr.report_dict(dict_report={'multi_objective': multi_obj}, step=epoch)
 
 
-def tune_run(config):
-    with open('/home/eugene/irontorch/configs/mnist_params.yaml') as f:
-        params = yaml.load(f, Loader=yaml.FullLoader)
+def tune_run(exp_name, search_space):
+    """
+    Tune the model and return the best model.
+    :param exp_name:
+    :param search_space:
+    :return:
+    """
+    callbacks = [WandbLoggerCallback(f"rayTune_{exp_name}",
+                                     excludes=["time_since_restore",
+                                               "training_iteration",
+                                               "warmup_time",
+                                               "iterations_since_restore",
+                                               "time_this_iter_s",
+                                               "time_total_s",
+                                               "timestamp",
+                                               "timesteps_since_restore"])]
 
-    for key, value in config.items():
-        if params.get(key, None) is not None:
-            params[key] = value
+    if name == 'so':
+        optuna_search = OptunaSearch(metric="accuracy", mode="max")
+        asha_scheduler = ASHAScheduler(time_attr='epoch', metric='accuracy',
+                                       mode='max', max_t=search_space['epoch'],
+                                       grace_period=search_space['grace_period'],
+                                       reduction_factor=4)
+    elif name == 'mo':
+        optuna_search = OptunaSearch(metric="multi_objective", mode="max")
+        asha_scheduler = ASHAScheduler(time_attr='epoch', metric='multi_objective',
+                                       mode='max', max_t=search_space['epoch'],
+                                       grace_period=search_space['grace_period'],
+                                       reduction_factor=4)
+    elif name == 'multi' and search_space['search_alg'] == 'optuna':
+        optuna_search = OptunaSearch(metric=["accuracy", "backdoor_accuracy"],
+                                     mode=["max", "min"])
+        asha_scheduler = None
+    else:
+        raise ValueError(name + search_space['search_alg'])
 
-    helper = Helper(params)
-    run(helper)
-    # main_obj, back_obj, multi_obj = list(), list(), list()
-    # for x in range(10):
-    #     params['random_seed'] = x
-    #     helper = Helper(params)
-    #     acc, back_acc, mo = run(helper)
-    #     main_obj.append(acc)
-    #     back_obj.append(back_acc)
-    #     multi_obj.append(mo)
-    #     if acc <= 80 or back_acc >= 90:
-    #         tune.report(accuracy=np.mean(main_obj),
-    #                     backdoor_accuracy=np.mean(back_obj),
-    #                     multi_objective=np.mean(multi_obj),
-    #                     acc_std=np.std(main_obj),
-    #                     back_std=np.std(back_obj)
-    #                     )
-    #         return
-    # tune.report(accuracy=np.mean(main_obj),
-    #             backdoor_accuracy=np.mean(back_obj),
-    #             multi_objective=np.mean(multi_obj),
-    #             acc_std=np.std(main_obj),
-    #             back_std=np.std(back_obj))
+    if search_space['search_alg'] == 'optuna':
+        asha_scheduler = None
+    else:
+        optuna_search = None
+
+    analysis = tune.run(run, config=search_space, num_samples=300,
+                    name=exp_name,
+                    search_alg=optuna_search,
+                    scheduler=asha_scheduler,
+                    resources_per_trial=tune.PlacementGroupFactory(
+                        [{"CPU": 4, "GPU": 1}]),
+                    log_to_file=True,
+                    fail_fast=True,
+                    callbacks=callbacks,
+                    keep_checkpoints_num=1,
+                    )
+    print(
+        "Best hyperparameters for accuracy found were: ",
+        analysis.get_best_config("accuracy", "min"),
+    )
+    print(
+        "Best hyperparameters for backdoor_accuracy found were: ",
+        analysis.get_best_config("backdoor_accuracy", "max"),
+    )
 
 
 if __name__ == '__main__':
 
-    ray.init(address='ray://128.84.84.162:10001', runtime_env={"working_dir": "/home/eugene/irontorch",
-                                                               'excludes': ['.git',
-                                                                            '.data']},
+    ray.init(address='ray://128.84.84.162:10001',
+             runtime_env={"working_dir": "/home/eugene/irontorch",
+                          'excludes': ['.git', '.data']},
              include_dashboard=True, dashboard_host='0.0.0.0')
 
-    for name in ['so', 'mo', 'multi']:
-        poisoning_proportion = 25
-        exp_name = f'optuna_{name}_mnist_{poisoning_proportion}'
+    for name in ['so', 'mo']:
+        poisoning_proportion = 200
+        search_alg = 'asha'
+        exp_name = f'{search_alg}_{name}_mnist_{poisoning_proportion}'
         search_space = {
             "optimizer": tune.choice(['SGD', 'Adam']),
             "lr": tune.loguniform(1e-5, 1e-1, 10),
@@ -102,56 +139,9 @@ if __name__ == '__main__':
             "batch_size": tune.choice([32, 64, 128, 256, 512]),
             # "drop_label_proportion": 0.95,
             "multi_objective_alpha": 0.95,
+            "search_alg": search_alg,
             "poisoning_proportion": poisoning_proportion,
+            "file_path": '/home/eugene/irontorch/configs/mnist_params.yaml',
 
         }
-        callbacks = [WandbLoggerCallback(f"rayTune_{exp_name}",
-                                         excludes=["time_since_restore",
-                                                   "training_iteration",
-                                                   "warmup_time",
-                                                   "iterations_since_restore",
-                                                   "time_this_iter_s",
-                                                   "time_total_s",
-                                                   "timestamp",
-                                                   "timesteps_since_restore"])]
-        # asha_scheduler = ASHAScheduler(
-        #     time_attr='epoch',
-        #     metric='multi_objective',
-        #     mode='max',
-        #     max_t=15,
-        #     grace_period=2,
-        #     reduction_factor=4,
-        # )
-        # hyperopt_search = HyperOptSearch(search_space, metric="multi_objective", mode="max")
-        if name == 'so':
-            optuna_search = OptunaSearch(metric="accuracy", mode="max")
-        elif name == 'mo':
-            optuna_search = OptunaSearch(metric="multi_objective", mode="max")
-        elif name == 'multi':
-            optuna_search = OptunaSearch(metric=["accuracy", "backdoor_accuracy"], mode=["max", "min"])
-        else:
-            raise ValueError(name)
-
-        analysis = tune.run(tune_run, config=search_space, num_samples=300,
-                        name=exp_name,
-                        # scheduler=asha_scheduler,
-                        search_alg=optuna_search,
-                        # resources_per_trial={'gpu': 1, 'cpu': 2},
-                        resources_per_trial=tune.PlacementGroupFactory([{"CPU": 4, "GPU": 1}]),
-                        log_to_file=True,
-                        fail_fast=True,
-                        callbacks=callbacks,
-                        keep_checkpoints_num=1,
-                        # sync_to_driver=False,
-                        # metric='multi_objective',
-                        # mode='max'
-                        )
-
-    print(
-        "Best hyperparameters for accuracy found were: ",
-        analysis.get_best_config("accuracy", "min"),
-    )
-    print(
-        "Best hyperparameters for backdoor_accuracy found were: ",
-        analysis.get_best_config("backdoor_accuracy", "max"),
-    )
+        tune_run(exp_name, search_space)
--- ray_training_cifar.py
+++ ray_training_cifar.py
@@ -15,137 +15,33 @@ from ray.tune.schedulers import ASHAScheduler
 import numpy as np
 import logging
 import functools
-logger = logging.getLogger('logger')
 
-def disable_logging(func):
-    @functools.wraps(func)
-    def wrapper(*args,**kwargs):
-        logging.disable(logging.DEBUG)
-        result = func(*args,**kwargs)
-        logging.disable(logging.NOTSET)
-        return result
-    return wrapper
-
-
-def run(hlpr):
-    # acc = test(hlpr, 0, backdoor=False)
-    for epoch in range(hlpr.params.start_epoch,
-                       hlpr.params.epochs + 1):
-        logging.disable(logging.DEBUG)
-        train(hlpr, epoch, hlpr.task.model, hlpr.task.optimizer,
-              hlpr.task.train_loader)
-        metrics = test(hlpr, hlpr.task.model, backdoor=False, epoch=epoch)
-        drop_class = hlpr.task.metrics['accuracy'].get_value()['_Accuracy_Drop_5']
-        backdoor_metrics = test(hlpr, hlpr.task.model, backdoor=True, epoch=epoch)
-        main_obj = metrics[hlpr.params.multi_objective_metric]
-        back_obj = backdoor_metrics[hlpr.params.multi_objective_metric]
-        alpha = hlpr.params.multi_objective_alpha
-        multi_obj = alpha * main_obj - (1 - alpha) * back_obj
-        tune.report(accuracy=main_obj, drop_class=drop_class,
-                    backdoor_accuracy=back_obj,
-                    multi_objective=multi_obj, epoch=epoch)
-    # return main_obj, back_obj, multi_obj
-     # main_obj, back_obj
-     #    hlpr.report_dict(dict_report={'multi_objective': multi_obj}, step=epoch)
-
-
-def tune_run(config):
-    with open('/home/eugene/irontorch/configs/cifar10_params.yaml') as f:
-        params = yaml.load(f, Loader=yaml.FullLoader)
-
-    for key, value in config.items():
-        if params.get(key, None) is not None:
-            params[key] = value
-            logger.error(f'Updating {key} with {value}')
-    helper = Helper(params)
-    run(helper)
-    # main_obj, back_obj, multi_obj = list(), list(), list()
-    # for x in range(10):
-    #     params['random_seed'] = x
-    #     helper = Helper(params)
-    #     acc, back_acc, mo = run(helper)
-    #     main_obj.append(acc)
-    #     back_obj.append(back_acc)
-    #     multi_obj.append(mo)
-    #     if acc <= 80 or back_acc >= 90:
-    #         tune.report(accuracy=np.mean(main_obj),
-    #                     backdoor_accuracy=np.mean(back_obj),
-    #                     multi_objective=np.mean(multi_obj),
-    #                     acc_std=np.std(main_obj),
-    #                     back_std=np.std(back_obj)
-    #                     )
-    #         return
-    # tune.report(accuracy=np.mean(main_obj),
-    #             backdoor_accuracy=np.mean(back_obj),
-    #             multi_objective=np.mean(multi_obj),
-    #             acc_std=np.std(main_obj),
-    #             back_std=np.std(back_obj))
-
+from ray_training import tune_run
 
 if __name__ == '__main__':
-    exp_name = 'cifar_100'
-    search_space = {
-        "optimizer": 'SGD',
-        "lr": tune.loguniform(1e-7, 3e-1, 10),
-        "scheduler": tune.choice([False, True]),
-        "momentum": tune.uniform(0, 1),
-        # "label_noise": tune.uniform(0.0, 0.3),
-        "decay": tune.loguniform(1e-7, 1e-1, 10),
-        "epochs": 200,
-        "batch_size": tune.grid_search([32, 64, 128, 256]),
-        # "drop_label_proportion": 0.95,
-        "multi_objective_alpha": 0.99,
-        "poisoning_proportion": 200,
-    }
-    asha_scheduler = ASHAScheduler(
-        time_attr='epoch',
-        metric='multi_objective',
-        mode='max',
-        max_t=200,
-        grace_period=10,
-        reduction_factor=3,
-    )
-    callbacks = [WandbLoggerCallback(f"rayTune_{exp_name}",
-                                     excludes=["time_since_restore", "training_iteration", "warmup_time",
-                                       "iterations_since_restore", "time_this_iter_s", "time_total_s",
-                                       "timestamp", "timesteps_since_restore"])
-    ]
-    # runtime_env = RuntimeEnv(
-    #     conda='pt',
-    #     working_dir="/home/eugene/irontorch",
-    #     # py_modules=[Helper, test, train]
-    #
-    # )
-    # hyperopt_search = HyperOptSearch(search_space, metric="multi_objective", mode="max")
-    # optuna_search = OptunaSearch(metric="accuracy", mode="max")
-    optuna_search = OptunaSearch(metric=["accuracy", "backdoor_accuracy"], mode=["max", "min"])
 
-    ray.init(address='ray://128.84.84.162:10001', runtime_env={"working_dir": "/home/eugene/irontorch",
-                                                               'excludes': ['.git',
-                                                                            '.data']},
+    ray.init(address='ray://128.84.84.162:10001',
+             runtime_env={"working_dir": "/home/eugene/irontorch",
+                          'excludes': ['.git', '.data']},
              include_dashboard=True, dashboard_host='0.0.0.0')
 
-    analysis = tune.run(tune_run, config=search_space, num_samples=100,
-                        name=exp_name,
-                        scheduler=asha_scheduler,
-                        # search_alg=optuna_search,
-                        # resources_per_trial={'gpu': 1, 'cpu': 2},
-                        resources_per_trial=tune.PlacementGroupFactory([{"CPU": 4, "GPU": 1}]),
-                        log_to_file=True,
-                        fail_fast=True,
-                        callbacks=callbacks,
-                        # max_failures=1,
-                        keep_checkpoints_num=1,
-                        # sync_to_driver=False,
-                        # metric='multi_objective',
-                        # mode='max'
-                        )
+    for name in ['so', 'mo']:
+        poisoning_proportion = 200
+        search_alg = 'asha'
+        exp_name = f'{search_alg}_{name}_mnist_{poisoning_proportion}'
+        search_space = {
+            "optimizer": tune.choice(['SGD', 'Adam']),
+            "lr": tune.loguniform(1e-5, 1e-1, 10),
+            "momentum": tune.uniform(0, 1),
+            # "label_noise": tune.uniform(0.0, 0.3),
+            "decay": tune.loguniform(1e-7, 1e-3, 10),
+            "epochs": 15,
+            "batch_size": tune.choice([32, 64, 128, 256, 512]),
+            # "drop_label_proportion": 0.95,
+            "multi_objective_alpha": 0.95,
+            "search_alg": search_alg,
+            "poisoning_proportion": poisoning_proportion,
+            "file_path": '/home/eugene/irontorch/configs/cifar10_params.yaml',
 
-    print(
-        "Best hyperparameters for accuracy found were: ",
-        analysis.get_best_config("accuracy", "min"),
-    )
-    print(
-        "Best hyperparameters for backdoor_accuracy found were: ",
-        analysis.get_best_config("backdoor_accuracy", "max"),
-    )
+        }
+        tune_run(exp_name, search_space)
