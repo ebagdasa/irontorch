60c9e501b60dd3939c08d57f47800f24439f1c00
Eugene <ebagdasaryan@me.com>
2022-06-13 21:32:28 -0400

    50

--- ray_training_cifar.py
+++ ray_training_cifar.py
@@ -26,10 +26,10 @@ if __name__ == '__main__':
              include_dashboard=True, dashboard_host='0.0.0.0')
 
     for name in ['so', 'mo', 'multi']:
-        poisoning_proportion = 200
+        poisoning_proportion = 50
         search_alg = 'optuna'
         exp_name = f'{search_alg}_{name}_cifar_{poisoning_proportion}'
-        max_iterations = 5
+        max_iterations = 100
         search_space = {
             "name": name,
             "optimizer": 'SGD',
