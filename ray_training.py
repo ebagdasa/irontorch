24652325c23d671b0e03dbcd39bdb7f337139cec
Eugene <ebagdasaryan@me.com>
2022-06-15 06:26:04 -0400

    mnist

--- ray_training.py
+++ ray_training.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
             "batch_size": tune.choice([32, 64, 128, 256]),
             # "transform_sharpness": tune.loguniform(1e-4, 1, 10),
             # "transform_erase": tune.loguniform(1e-4, 1, 10),
-            "grad_sigma": tune.qloguniform(1e-5, 1e-1, 10, 5e-6),
+            "grad_sigma": tune.qloguniform(1e-5, 1e-1, 5e-6),
             # "grad_clip": tune.loguniform(1e-1, 10, 10),
             "label_noise": tune.quniform(0.0, 0.5, 0.1),
             # "drop_label_proportion": 0.95,
