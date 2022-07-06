c1681524698177ff494401f149f44647f27b83b4
Eugene <ebagdasaryan@me.com>
2022-07-06 14:46:12 -0400

    cifar

--- ray_training_cifar.py
+++ ray_training_cifar.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
         exp_name = f'cifar_run_normal2'
         max_iterations = 80
         search_space = {
-            "name": 'normal',
-            "batch_size": 128,
+            "name": 'robust',
+            "batch_size": 64,
             # "optimizer": tune.choice(['SGD', 'Adam']),
             # # "scheduler": tune.choice([True, False]),
             # "grace_period": 2,
