d09e0c3c666a33010c751da7bec4f0238f861da6
Eugene <ebagdasaryan@me.com>
2022-07-14 22:22:43 -0400

    stages

--- ray_training.py
+++ ray_training.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':
         "max_iterations": max_iterations
     }
 
-    analysis = tune_run(full_exp_name, search_space, resume=False)
+    analysis = tune_run(full_exp_name, search_space, resume=True)
     print('Finished stage 3 tuning.')
 
     # stage 4
