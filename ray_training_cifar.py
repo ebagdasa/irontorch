1b6cb9dc4d77f44cfe0d72b6c7069345cc20d6d4
Eugene <ebagdasaryan@me.com>
2022-07-16 08:45:14 -0400

    stages

--- ray_training_cifar.py
+++ ray_training_cifar.py
@@ -11,6 +11,8 @@ if __name__ == '__main__':
     parser.add_argument('--random_seed', default=None, type=int)
     parser.add_argument('--backdoor_label', default=None, type=int)
     parser.add_argument('--poisoning_proportion', default=None, type=float)
+    parser.add_argument('--skip_stage3', default=False, type=bool)
+    parser.add_argument('--exp_name', default=None, type=str)
 
     args = parser.parse_args()
 
@@ -18,13 +20,13 @@ if __name__ == '__main__':
              runtime_env={"working_dir": "/home/eugene/irontorch",
                           'excludes': ['.git', '.data']},
              include_dashboard=True, dashboard_host='0.0.0.0')
-
+    backdoor_cover_percentage = 0.01
     search_alg = 'optuna'
     exp_name = f'cifar_{search_alg}_it1'
     if args.random_seed is None and args.backdoor_label is None:
         # stage 1
         print('Running stage 1')
-        group_name = 'stage1'
+        group_name = f'stage1_{exp_name}'
         max_iterations = 50
         full_exp_name = f'{exp_name}_{group_name}'
         search_space = {
@@ -33,7 +35,7 @@ if __name__ == '__main__':
             'random_seed': tune.choice(list(range(0, 50))),
             'backdoor_label': tune.choice(list(range(0, 10))),
             'epochs': 2,
-            'backdoor_cover_percentage': 0.01,
+            'backdoor_cover_percentage': backdoor_cover_percentage,
             'search_alg': None,
             'poisoning_proportion': 0,
             'file_path': '/home/eugene/irontorch/configs/cifar10_params.yaml',
@@ -60,13 +62,14 @@ if __name__ == '__main__':
         # stage 2
         print('Running stage 2')
         max_iterations = 40
-        group_name = 'stage2'
+        group_name = f'stage2_{exp_name}'
         full_exp_name = f'{exp_name}_{group_name}'
         search_space = {
             'wandb_name': exp_name,
             'group': group_name,
             'random_seed': random_seed,
             'backdoor_label': backdoor_label,
+            'backdoor_cover_percentage': backdoor_cover_percentage,
             'epochs': 30,
             'search_alg': None,
             'poisoning_proportion': tune.qrandint(0, 200, q=5),
@@ -87,47 +90,51 @@ if __name__ == '__main__':
         poisoning_proportion = args.poisoning_proportion
 
     # stage 3
-    print('Running stage 3')
-    search_alg = 'optuna'
-    group_name = 'stage3_3'
-    metric_name = 'multi'
-    max_iterations = 500
-    full_exp_name = f'{exp_name}_{group_name}'
-    search_space = {
-        "metric_name": metric_name,
-        'wandb_name': exp_name,
-        "group": group_name,
-        "optimizer": tune.choice(['SGD', 'Adam']),
-        "scheduler": False,
-        "grace_period": 2,
-        "lr": tune.qloguniform(1e-4, 2e-1, 1e-5, base=10),
-        "momentum": tune.quniform(0.1, 1.0, 0.05),
-        "decay": tune.qloguniform(1e-7, 1e-3, 1e-7, base=10),
-        "epochs": 30,
-        "batch_size": tune.choice([64, 128, 256]),
-        # "drop_label_proportion": 0.95,
-        "multi_objective_alpha": 0.95,
-        "search_alg": search_alg,
-        "grad_sigma": tune.qloguniform(1e-6, 1e-1, 1e-6, base=10),
-        "grad_clip": tune.qloguniform(1, 32, 1, base=2),
-        "label_noise": tune.quniform(0.0, 0.7, 0.01),
-        "poisoning_proportion": poisoning_proportion,
-        "file_path": '/home/eugene/irontorch/configs/cifar10_params.yaml',
-        "max_iterations": max_iterations
-    }
+    if not args.skip_stage3:
+        print('Running stage 3')
+        search_alg = 'optuna'
+        group_name = f'stage3_{exp_name}'
+        metric_name = 'multi'
+        max_iterations = 500
+        full_exp_name = f'{exp_name}_{group_name}'
+        search_space = {
+            "metric_name": metric_name,
+            'wandb_name': exp_name,
+            "group": group_name,
+            "optimizer": tune.choice(['SGD', 'Adam']),
+            "scheduler": False,
+            'random_seed': random_seed,
+            'backdoor_label': backdoor_label,
+            'backdoor_cover_percentage': backdoor_cover_percentage,
+            "poisoning_proportion": poisoning_proportion,
+            "grace_period": 2,
+            "lr": tune.qloguniform(1e-4, 2e-1, 1e-5, base=10),
+            "momentum": tune.quniform(0.1, 1.0, 0.05),
+            "decay": tune.qloguniform(1e-7, 1e-3, 1e-7, base=10),
+            "epochs": 30,
+            "batch_size": tune.choice([64, 128, 256]),
+            # "drop_label_proportion": 0.95,
+            "multi_objective_alpha": 0.95,
+            "search_alg": search_alg,
+            "grad_sigma": tune.qloguniform(1e-6, 1e-1, 1e-6, base=10),
+            "grad_clip": tune.qloguniform(1, 32, 1, base=2),
+            "label_noise": tune.quniform(0.0, 0.7, 0.01),
+            "file_path": '/home/eugene/irontorch/configs/cifar10_params.yaml',
+            "max_iterations": max_iterations
+        }
 
-    analysis = tune_run(full_exp_name, search_space, resume=False)
-    print('Finished stage 3 tuning.')
+        analysis = tune_run(full_exp_name, search_space, resume=False)
+        print('Finished stage 3 tuning.')
 
-    # stage 4
-    print('Running stage 4')
-    group_name = 'stage4'
-    full_exp_name = f'{exp_name}_{group_name}'
-    config = analysis.get_best_config("multi_objective", "max")
-    print(config)
-    config['group'] = group_name
-    config['poisoning_proportion'] = tune.qrandint(0, 200, q=5)
-    config['max_iterations'] = 100
-    config['search_alg'] = None
-    tune_run(full_exp_name, config)
+        # stage 4
+        print('Running stage 4')
+        group_name = f'stage4_{exp_name}'
+        full_exp_name = f'{exp_name}_{group_name}'
+        config = analysis.get_best_config("multi_objective", "max")
+        print(config)
+        config['group'] = group_name
+        config['poisoning_proportion'] = tune.qrandint(0, 200, q=5)
+        config['max_iterations'] = 100
+        config['search_alg'] = None
+        tune_run(full_exp_name, config)
 
