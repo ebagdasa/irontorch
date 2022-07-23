4970ef81bad0661dae0d54b94c11b5c45781956b
Eugene <ebagdasaryan@me.com>
2022-07-22 21:04:55 -0400

    synthesizer

--- utils/parameters.py
+++ utils/parameters.py
@@ -94,8 +94,8 @@ class Params:
     # `backdoor_multi`.
     loss_tasks: List[str] = None
 
-    cifar_model_l1: int = None
-    cifar_model_l2: int = None
+    cifar_model_l1: int = None # 120
+    cifar_model_l2: int = None # 84
 
     transform_erase: float = 0.0
     transform_sharpness: float = 0.0
