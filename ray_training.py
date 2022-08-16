21060cfb60629ae757317fd0d69e47856407aead
Eugene <ebagdasaryan@me.com>
2022-08-15 20:42:52 -0400

    imagenet

--- ray_training.py
+++ ray_training.py
@@ -240,7 +240,7 @@ if __name__ == '__main__':
         epochs = 5
         proportion_to_test = np.unique(np.logspace(0, 10, num=40, base=2, dtype=np.int32)).tolist()
     elif args.task == 'imagenet':
-        epochs = 10
+        epochs = 5
         proportion_to_test = np.unique(np.logspace(0, 10, num=40, base=2, dtype=np.int32)).tolist()
     else:
         raise ValueError(f'Unknown task {args.task}')
