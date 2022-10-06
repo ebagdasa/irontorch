74c520960fc7b85b02638e78b57816a799cd0ac8
Eugene <ebagdasaryan@me.com>
2022-10-06 14:12:23 -0400

    fl

--- ray_training.py
+++ ray_training.py
@@ -274,8 +274,8 @@ if __name__ == '__main__':
     if args.task == 'mnist':
         epochs = 5
         proportion_to_test = [5*i for i in range(36)] #np.unique(np.logspace(0, 10, num=80, base=2, dtype=np.int32)).tolist()
-        proportions = {'SinglePixel': 10, 'Dynamic': 14, 'Pattern': 9, 'Primitive': 9,
-                       'Complex': 14, 'Clean': 13}
+        proportions = {'SinglePixel': 12, 'Dynamic': 14, 'Pattern': 12, 'Primitive': 12,
+                       'Complex': 14, 'Clean': 15}
     elif args.task == 'cifar10':
         epochs = 10
         proportion_to_test = np.unique(np.logspace(0, 10, num=27, base=2, dtype=np.int32)).tolist()
