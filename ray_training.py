cf17e7b974337ec912d2331abd9dc4618dd135e9
Eugene <ebagdasaryan@me.com>
2022-10-21 13:30:19 -0400

    fl

--- ray_training.py
+++ ray_training.py
@@ -164,7 +164,7 @@ def tune_run(exp_name, search_space, resume=False):
                         fail_fast=False,
                         callbacks=callbacks,
                         keep_checkpoints_num=1,
-                        resume='PROMPT',
+                        resume=True,
                         raise_on_failed_trial=False,
                         max_failures=10
                         )
