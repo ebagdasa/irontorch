f577d5fe040f9b947b00f42c7d2628be5b2e01c5
Eugene <ebagdasaryan@me.com>
2022-10-21 14:24:50 -0400

    fl

--- ray_training.py
+++ ray_training.py
@@ -164,9 +164,7 @@ def tune_run(exp_name, search_space, resume=False):
                         fail_fast=False,
                         callbacks=callbacks,
                         keep_checkpoints_num=1,
-                        resume=True,
-                        raise_on_failed_trial=False,
-                        max_failures=10
+                        resume=resume
                         )
     print(
         "Best hyperparameters for accuracy found were: ",
