4ddc968f332b6b2bdbafe596307b744f7adc07a4
Eugene <ebagdasaryan@me.com>
2022-11-24 13:11:52 -0500

    fl

--- ray_training.py
+++ ray_training.py
@@ -569,7 +569,7 @@ if __name__ == '__main__':
 
 
     if args.stage4_multi_backdoor:
-        synthesizers = ['Primitive', 'SinglePixel',  'Complex'] # 'Dynamic', 'Pattern',  'Clean'
+        synthesizers = ['Primitive', 'SinglePixel',  'Complex', 'Dynamic', 'Clean']
     else:
         synthesizers = [args.synthesizer]
 
--- synthesizers/clean_synthesizer.py
+++ synthesizers/clean_synthesizer.py
@@ -1,8 +1,16 @@
 from synthesizers.pattern_synthesizer import PatternSynthesizer
-
+import torch
 
 class CleanSynthesizer(PatternSynthesizer):
     name = 'Clean'
+
+    def make_pattern(self):
+        full_image = torch.zeros_like(self.input_stats.average_input_values)
+        full_image.fill_(0)
+        full_image.view(-1)[::2] = 1
+
+        self.mask = torch.ones_like(self.input_stats.average_input_values) * 0.1
+        self.pattern = full_image * self.input_stats.max_val.max()
 
     def get_label(self, input_tensor, target_tensor):
         target_label = self.params.backdoor_labels[self.name]
